f2d7dbbbb89ab4918b1477e053c27be43b1fc108
Marco Budiongan <marcobdngn@gmail.com>
2022-03-12 22:06:39 +0800

    chore: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { AppRoutes } from "./routes";
+import { QueryClientProvider, QueryClient } from "react-query";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
